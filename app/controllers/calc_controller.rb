5dd4b69fa22840b2d8cc67d3adbb1bcf502af3fa
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-27 01:56:57 +0000

    payment calc done

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -19,5 +19,28 @@ class CalcController < ApplicationController
     @the_root = @the_num ** 0.5
     render({ :template => "calc_templates/squarerootresults"})
   end
-  
+
+  def payment
+    render({ :template => "calc_templates/paymentnew"})
+  end
+
+  def paymentresults
+    @apr = params.fetch("user_apr").to_f
+    @years = params.fetch("user_years").to_f
+    @principal = params.fetch("user_pv").to_f
+
+    @formatted_apr = format('%.4f%%', @apr)
+    @formatted_years = @years.to_i
+    @formatted_principal = @principal.to_fs(:currency)
+
+    @r = @apr/(100*12)
+    @n = @years * 12
+    @numerator = @r * @principal
+    @denominator = 1 - ((1 + @r) ** (-@n))
+    @payment = @numerator/@denominator
+
+    @formatted_payment = @payment.to_fs(:currency)
+
+    render({ :template => "calc_templates/paymentresults"})
+  end
 end
