32560e38c41c25a5d1765f51fa78542de48c2314
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-27 01:57:32 +0000

    payment

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   get("/", { :controller => "calc", :action => "home"})
 
   get("/square/new", { :controller => "calc", :action => "home"})
-
   get("/square/results", { :controller => "calc", :action => "square"})
 
   get("/square_root/new", { :controller => "calc", :action => "squareroot"})
-
   get("/square_root/results", { :controller => "calc", :action => "squarerootresults"})
 
   get("/payment/new", { :controller => "calc", :action => "payment"})
-
   get("/payment/results", { :controller => "calc", :action => "paymentresults"})
 end
