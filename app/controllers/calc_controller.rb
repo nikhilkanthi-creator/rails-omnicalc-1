b12ca1d8783b6e816cb7a0cfc624faf6f102e7e0
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-27 01:45:15 +0000

    first one working

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -3,4 +3,11 @@ class CalcController < ApplicationController
   def home
     render({ :template => "calc_templates/home"})
   end
+
+  def square
+    @the_num = params.fetch("input_var").to_f
+    @the_square = @the_num * @the_num
+    render({ :template => "calc_templates/square"})
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "calc", :action => "home"})
+
+  get("/square/results", { :controller => "calc", :action => "square"})
+
+  get("/square/new", { :controller => "calc", :action => "home"})
 end
