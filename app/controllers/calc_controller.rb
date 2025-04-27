d0411251cd11708890fc84de1b22bc77c6decc35
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-27 01:52:03 +0000

    square root working

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -9,5 +9,15 @@ class CalcController < ApplicationController
     @the_square = @the_num * @the_num
     render({ :template => "calc_templates/square"})
   end
+
+  def squareroot
+    render({ :template => "calc_templates/squarerootnew"})
+  end
+
+  def squarerootresults
+    @the_num = params.fetch("input_var").to_f
+    @the_root = @the_num ** 0.5
+    render({ :template => "calc_templates/squarerootresults"})
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "calc", :action => "home"})
 
+  get("/square/new", { :controller => "calc", :action => "home"})
+
   get("/square/results", { :controller => "calc", :action => "square"})
 
-  get("/square/new", { :controller => "calc", :action => "home"})
+  get("/square_root/new", { :controller => "calc", :action => "squareroot"})
+
+  get("/square_root/results", { :controller => "calc", :action => "squarerootresults"})
 end
